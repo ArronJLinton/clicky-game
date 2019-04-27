d4125eba461ed687a7b60bf943b3dc4f41a36e2b
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-27 16:54:24 -0700

    Classified Board

--- src/components/board.js
+++ src/components/board.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Card from "./card";
 
-function Board(props) {
+class Board extends Component {
 
-    let cards = []
-    return(
+    randomize = (arr) => {
+        for (let i = arr.length - 1; i > 0; i--){
+            let j = Math.floor(Math.random() * (i+1));
+            let temp = arr[i]
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    };
+
+    updateCards = () => {
+        let images = [
+            "https://i.pinimg.com/564x/b2/71/a4/b271a401ea2305f35604581ee1f09dd6--rick-y-morty-rick-and-morty-portal.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz5FtQvvaalMjwwPs7aroO2N-Umq_dQoW6MpkIu6-n8Nh9rLIu",
+            "https://images-na.ssl-images-amazon.com/images/I/61mv11Bd6vL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81cXzPwn2qL.jpg",
+            "https://colinsreview.files.wordpress.com/2017/12/rick-and-morty-the-ricks-must-be-crazy-square.jpg?w=720",
+            "https://target.scene7.com/is/image/Target/GUEST_00aadfd4-45f4-4363-8044-874912610773?wid=488&hei=488&fmt=pjpeg",
+            "https://spotlight.radiopublic.com/images/thumbnail?url=https%3A%2F%2Fmedia.podiant.co%2Fspoke%2Fcinescapemagazine%2Fartwork%2F5a22f6c59ae04.jpg"
+        ]
+        this.randomize(images);
+        return images.map(card => <Card src={card} name={"Rick and Morty"}/>);
+    }
+
+    state ={
+        cards: this.updateCards()
+    };
+
+    render() {
+    return (
         <div className="row">
-            <div className="col s12">
-                <div className="card-panel teal">
-
-                </div>
+            <div className="col">
+                {this.state.cards}
             </div>
         </div>
-    );
+    )};
 };
 
 export default Board
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,8 @@
 import React from 'react';
 
 function Card(props) {
-
-    return(
-        <div className="row">
-            <div className="col s12 m5">
-                <div className="card-panel teal">
-        <span className="white-text">I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-        </span>
-                </div>
-            </div>
-        </div>
-    );
+    return(<img style={{height:200, width:200, margin:6.5}} src={props.src} alt={props.name}/>);
+//    TODO: Add data-clicked to default as false
 };
 
 export default Card
