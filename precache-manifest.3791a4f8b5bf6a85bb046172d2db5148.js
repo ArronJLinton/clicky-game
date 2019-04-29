f5f0f49509c3b76892ff1de4b3f62e108d5dfd87
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-29 10:07:05 -0700

    Updates

---
+++ precache-manifest.3791a4f8b5bf6a85bb046172d2db5148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae43312b77c9f89ab1509204c07cffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9218f84349b71eba2e88",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9218f84349b71eba2e88",
+    "url": "/static/js/main.841393d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
